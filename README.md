59eda9daba738bfb1d63826a52d9e97a726658da
Poison02 <2069820192@qq.com>
2024-04-07 21:34:04 +0800

    feat: new project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Todo List
+这是一个使用Gin + Gorm + Vue实现的小项目，一个TodoList。
---
+++ dao/mysql.go
@@ -0,0 +1,19 @@
+package dao
+
+import (
+	"fmt"
+	"go-todos/setting"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func InitMySQL(cfg *setting.MySQLConfig) (err error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DB)
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	return err
+}
